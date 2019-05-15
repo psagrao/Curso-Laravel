d4a2801730a8b478e8a897586a281c7e84020d56
Yusnier <yreyes@grao.com>
2019-05-15 18:25:50 +0200

    router

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//lo que se retorna es la vista a mostrar
 Route::get('/', function () {
     return view('welcome');
 });
+
